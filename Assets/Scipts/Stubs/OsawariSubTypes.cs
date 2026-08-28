cb67fe9dd96cc1404eb9030a46733909ef67e087
nao <naoetc1234@yahoo.co.jp>
2026-08-28 20:10:55 +0900

    エラー42

--- Assets/Scipts/Stubs/OsawariSubTypes.cs
+++ Assets/Scipts/Stubs/OsawariSubTypes.cs
@@ -1,28 +1,68 @@
 using System;
+using Stubs;
+public class OsawariPiston : AbstractOsawari
+{
+    public IObservable<bool> OnInsert() { return new UniRx.Subject<bool>(); }
+    public IObservable<bool> OnPiston() { return new UniRx.Subject<bool>(); }
+
+    protected override void InitializeParams() { }
+    protected override void AutoAnimation() { }
+    protected override void UpdateWhileNotClicked() { }
+    protected override void UpdateParamsCore(UnityEngine.Vector3 move) { }
+    protected override int GetHandParamIndex(HandType handType) { return 0; }
+    protected override void OnLateUpdate() { }
+}
+
+public class OsawariKiss : AbstractOsawari
+{
+    public bool IsKissing;
+    public UniRx.Subject<UniRx.Unit> OnKissStart = new UniRx.Subject<UniRx.Unit>();
+    public UniRx.Subject<UniRx.Unit> OnKissEnd = new UniRx.Subject<UniRx.Unit>();
+
+    protected override void InitializeParams() { }
+    protected override void AutoAnimation() { }
+    protected override void UpdateWhileNotClicked() { }
+    protected override void UpdateParamsCore(UnityEngine.Vector3 move) { }
+    protected override int GetHandParamIndex(HandType handType) { return 0; }
+    protected override void OnLateUpdate() { }
+}
+
+public class OsawariWithoutHand : AbstractOsawari
+{
+    protected override void InitializeParams() { }
+    protected override void AutoAnimation() { }
+    protected override void UpdateWhileNotClicked() { }
+    protected override void UpdateParamsCore(UnityEngine.Vector3 move) { }
+    protected override int GetHandParamIndex(HandType handType) { return 0; }
+    protected override void OnLateUpdate() { }
+}
+
+public class OsawariDoublehanded : AbstractOsawari
+{
+    protected override void InitializeParams() { }
+    protected override void AutoAnimation() { }
+    protected override void UpdateWhileNotClicked() { }
+    protected override void UpdateParamsCore(UnityEngine.Vector3 move) { }
+    protected override int GetHandParamIndex(HandType handType) { return 0; }
+    protected override void OnLateUpdate() { }
+}
+
+public class OsawariPants : AbstractOsawari
+{
+    public bool IsAbleToInsert() { return false; }
+
+    protected override void InitializeParams() { }
+    protected override void AutoAnimation() { }
+    protected override void UpdateWhileNotClicked() { }
+    protected override void UpdateParamsCore(UnityEngine.Vector3 move) { }
+    protected override int GetHandParamIndex(HandType handType) { return 0; }
+    protected override void OnLateUpdate() { }
+}
+
+public class OsawariGoods : UnityEngine.MonoBehaviour { }
 
 namespace Stubs
 {
-    public class OsawariPiston : AbstractOsawari
-    {
-        public IObservable<bool> OnInsert() { return new UniRx.Subject<bool>(); }
-        public IObservable<bool> OnPiston() { return new UniRx.Subject<bool>(); }
-    }
-    public class OsawariKiss : AbstractOsawari
-    {
-        public bool IsKissing;
-        public UniRx.Subject<UniRx.Unit> OnKissStart = new UniRx.Subject<UniRx.Unit>();
-        public UniRx.Subject<UniRx.Unit> OnKissEnd = new UniRx.Subject<UniRx.Unit>();
-    }
-
-    public class OsawariWithoutHand : AbstractOsawari { }
-    public class OsawariDoublehanded : AbstractOsawari { }
-    public class OsawariPants : AbstractOsawari
-    {
-        public bool IsAbleToInsert() { return false; }
-    }
-
-    public class OsawariGoods : UnityEngine.MonoBehaviour { }
-
     public interface IWearable
     {
         bool IsWearing();
