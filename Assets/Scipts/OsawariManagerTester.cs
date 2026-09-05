f7cc94fd53423145a9be903c0dd4be3960f5d981
nao <naoetc1234@yahoo.co.jp>
2026-09-05 16:24:56 +0900

    タッチイベントメッシュを設定OK

--- Assets/Scipts/OsawariManagerTester.cs
+++ Assets/Scipts/OsawariManagerTester.cs
@@ -6,55 +6,28 @@ public class OsawariManagerTester : MonoBehaviour
 {
     public OsawariManager TargetOsawariManager;
 
-    void Start()
-    {
-        Debug.Log("=== OsawariManager 検証開始 ===");
+   void Start()
+{
+    Debug.Log("=== OsawariManager 検証開始 ===");
 
-        TargetOsawariManager.Raycasters = new Stubs.ContextRaycasterList();
-        TargetOsawariManager.Scene = new Stubs.HScene();
+    TargetOsawariManager.Raycasters = new Stubs.ContextRaycasterList();
+    TargetOsawariManager.Scene = new Stubs.HScene();
 
-        // ← 追加: HandManagerが未セットならここで作る(保険)
-        TargetOsawariManager.HandManager ??= new Stubs.HandManager();
+    TargetOsawariManager.HandManager ??= new Stubs.HandManager();
 
-        var scene = TargetOsawariManager.GetScene();
-        var raycaster = TargetOsawariManager.GetCubismRaycaster();
+    var scene = TargetOsawariManager.GetScene();
+    var raycaster = TargetOsawariManager.GetCubismRaycaster();
 
-        // モデルからDrawable(パーツ)を全部取得する
-        var drawables = TargetOsawariManager.Model.Drawables;
+    var drawables = TargetOsawariManager.Model.Drawables;
 
-        // 本物のOsawariHeadを取得(あらかじめGameObjectにアタッチしておく)
-        var osawariHead = TargetOsawariManager.GetComponent<OsawariHead>();
+    var osawariHead = TargetOsawariManager.GetComponent<OsawariHead>();
 
+    var targetList = new System.Collections.Generic.List<AbstractOsawari> { osawariHead };
+    TargetOsawariManager.ContextOsawariTargets = new Stubs.ContextOsawariTargetList();
+    TargetOsawariManager.ContextOsawariTargets.RegisterTargets(targetList);
 
-        // ContextOsawariTargets に、OsawariHeadを登録する
-        var targetList = new System.Collections.Generic.List<AbstractOsawari> { osawariHead };
-        TargetOsawariManager.ContextOsawariTargets = new Stubs.ContextOsawariTargetList();
-        TargetOsawariManager.ContextOsawariTargets.RegisterTargets(targetList);
-
-        var dummyEventObj = new GameObject("DummyTouchEvent");
-        
-        var dummyEvent = dummyEventObj.AddComponent<DummyTouchEvent>();
-        dummyEvent.StatusObject = FindObjectOfType<Stubs.StatusObject>();   // ← この1行を追加
-
-        osawariHead.OnTouchEvents.Add(dummyEvent);
-        // ← 追加: ここでManagedStart()を呼ぶ(これが今回追加したかった1行)
-        // osawariHead.ManagedStart(TargetOsawariManager, System.Threading.CancellationToken.None);
-        try
-        {
-            osawariHead.ManagedStart(TargetOsawariManager, System.Threading.CancellationToken.None);
-        }
-        catch (System.Exception e)
-        {
-            Debug.LogException(e);
-        }
-
-
-
-        // ← 移動: ManagedStart()の中でTouchableMeshsが上書きされるので、その後にセットし直す
-        osawariHead.TouchableMeshs = drawables;
-        Debug.Log(osawariHead.TouchableMeshs.Length + "個の全メッシュを登録しました");
-    }
-
+    osawariHead.ManagedStart(TargetOsawariManager, System.Threading.CancellationToken.None);
+}
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
